297961e9af796845d8a80a6d954b3999ff05fcb3
Eadgeez <Maxencemenard@hotmail.fr>
2024-12-23 00:22:13 +0100

    Fix: User fixtures (password hash)

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,71 +2,77 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use App\Tests\Fixtures\ThereIs;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
     public function load(ObjectManager $manager): void
     {
         $createdAt = new \DateTimeImmutable();
         $updatedAt = $createdAt->modify('+1 day');
         $deletedAt = $updatedAt->modify('+1 day');
 
-        // Create a super admin with the ROLE_SUPER_ADMIN role
-        ThereIs::aUser()
-            ->withUsername('Super Admin')
-            ->withEmail('super.admin@eureka.fr')
-            ->withPassword('super_admin_eureka')
-            ->withRoles(['ROLE_SUPER_ADMIN'])
-            ->withCreatedAt($createdAt)
-            ->withUpdatedAt($updatedAt)
-            ->withDeletedAt($deletedAt)
-            ->build();
+        $superAdmin = new User();
+        $superAdmin->setUsername('Super Admin');
+        $superAdmin->setEmail('super.admin@admineureka.fr');
+        $password = $this->hasher->hashPassword($superAdmin, 'super_admin_eureka');
+        $superAdmin->setPassword($password);
+        $superAdmin->setRoles(['ROLE_SUPER_ADMIN']);
+        $superAdmin->setCreatedAt($createdAt);
 
-        // Create an admin with the ROLE_ADMIN role
-        ThereIs::aUser()
-            ->withUsername('Admin')
-            ->withEmail('admin@eureka.fr')
-            ->withPassword('admin_eureka')
-            ->withRoles(['ROLE_ADMIN'])
-            ->withCreatedAt($createdAt)
-            ->withUpdatedAt($updatedAt)
-            ->withDeletedAt($deletedAt)
-            ->build();
+        $manager->persist($superAdmin);
 
-        // Create a teacher with the ROLE_TEACHER role
-        ThereIs::aUser()
-            ->withUsername('Teacher')
-            ->withEmail('teacher@eureka.fr')
-            ->withPassword('teacher_eureka')
-            ->withRoles(['ROLE_TEACHER'])
-            ->withCreatedAt($createdAt)
-            ->withUpdatedAt($updatedAt)
-            ->withDeletedAt($deletedAt)
-            ->build();
+        $admin = new User();
+        $admin->setUsername('Admin');
+        $admin->setEmail('admin@eureka.fr');
+        $password = $this->hasher->hashPassword($admin, 'admin_eureka');
+        $admin->setPassword($password);
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setCreatedAt($createdAt);
 
-        // Create a student with the ROLE_STUDENT role
-        ThereIs::aUser()
-            ->withUsername('Student')
-            ->withEmail('student@eureka.fr')
-            ->withPassword('student_eureka')
-            ->withRoles(['ROLE_STUDENT'])
-            ->withCreatedAt($createdAt)
-            ->withUpdatedAt($updatedAt)
-            ->withDeletedAt($deletedAt)
-            ->build();
+        $manager->persist($admin);
 
-        // Create a banned user with the ROLE_BANNED role
-        ThereIs::aUser()
-            ->withEmail('banned@eureka.fr')
-            ->withPassword('banned_eureka')
-            ->withRoles(['ROLE_BANNED'])
-            ->withCreatedAt($createdAt)
-            ->withUpdatedAt($updatedAt)
-            ->withDeletedAt($deletedAt)
-            ->build();
+        $teacher = new User();
+        $teacher->setUsername('Teacher');
+        $teacher->setEmail('teacher@eureka.fr');
+        $password = $this->hasher->hashPassword($teacher, 'teacher_eureka');
+        $teacher->setPassword($password);
+        $teacher->setRoles(['ROLE_TEACHER']);
+        $teacher->setCreatedAt($createdAt);
+
+        $manager->persist($teacher);
+
+        $student = new User();
+        $student->setUsername('Student');
+        $student->setEmail('student@eureka.fr');
+        $password = $this->hasher->hashPassword($student, 'student_eureka');
+        $student->setPassword($password);
+        $student->setRoles(['ROLE_STUDENT']);
+        $student->setCreatedAt($createdAt);
+
+        $manager->persist($student);
+
+        $banned = new User();
+        $banned->setUsername('Banned');
+        $banned->setEmail('banned@eureka.fr');
+        $password = $this->hasher->hashPassword($banned, 'banned_eureka');
+        $banned->setPassword($password);
+        $banned->setRoles(['ROLE_BANNED']);
+        $banned->setCreatedAt($createdAt);
+
+        $manager->persist($banned);
+
+        $manager->flush();
 
         // Create 10 users with the ROLE_USER role
         for ($i = 0; $i < 10; $i++) {
